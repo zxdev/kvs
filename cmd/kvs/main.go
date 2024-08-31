b7345ab5c0286d644c5ceea3bbc2339ca5ccc92a
zxdev <24780673+zxdev@users.noreply.github.com>
2024-08-31 15:07:41 -0700

    kvs: auto sense file extensions

--- cmd/kvs/main.go
+++ cmd/kvs/main.go
@@ -23,6 +23,7 @@ func main() {
 
 	case 2:
 
+		// sense the kvs file extensions for user convienence
 		info := kvs.Info(os.Args[1])
 		if !info.Ok {
 			info = kvs.Info(os.Args[1] + ".keon")
@@ -55,18 +56,31 @@ func main() {
 
 	case 3:
 
+		// the header signature is what determines the kvs structure type and
+		// not the kvs file extension which is sensed for convienence only
 		info := kvs.Info(os.Args[1])
 		if !info.Ok {
-			fmt.Println("kvs: invalid resource")
-			return
+			info = kvs.Info(os.Args[1] + ".keon")
+			if info.Ok {
+				os.Args[1] += ".keon"
+			} else {
+				info = kvs.Info(os.Args[1] + ".keva")
+				if info.Ok {
+					os.Args[1] += ".keva"
+				} else {
+					fmt.Println("kvs: invalid resource")
+					return
+				}
+			}
 		}
+
 		switch info.Signature {
 		case 0xff01: // keon
 			kv, ok := kvs.LoadKEON(os.Args[1])
 			if ok {
 				lookup := kv.Lookup()
 				for _, v := range strings.Split(os.Args[2], ",") {
-					fmt.Println("lookup:", v, lookup([]byte(v)))
+					fmt.Println("keon:", v, lookup([]byte(v)))
 				}
 			}
 
@@ -79,10 +93,10 @@ func main() {
 					var b [8]byte
 					binary.LittleEndian.PutUint64(b[:], item.Value)
 					if item.Value == 0 {
-						fmt.Printf("lookup: %s %v\n", v, item.Ok)
+						fmt.Printf("keva: %s %v\n", v, item.Ok)
 						continue
 					}
-					fmt.Printf("lookup: %s %v %v\n", v, item.Ok, b)
+					fmt.Printf("keva: %s %v %v\n", v, item.Ok, b)
 				}
 			}
 		}
